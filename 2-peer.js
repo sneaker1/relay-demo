ffc70d0ec31e8a2e20bcfd46320d2bccabb3a7e4
Matthias Schramm <matze.schramm@googlemail.com>
2022-08-12 20:56:44 +0200

    sync

--- 2-peer.js
+++ 2-peer.js
@@ -98,6 +98,12 @@ async function init() {
       var peerInfo = await node.peerStore.get(conn.remotePeer);
       var answer = await disc.getPeers(node, conn.remotePeer.toString());
       //console.log(answer);
+      for(var i=0; i<answer.answer.length; i++) {
+        if(answer.answer[i] !== node.peerId.toString()) {
+          console.log("Dialing: " + answer.answer[i]);
+          await node.dial("/ip4/89.58.0.139/tcp/15002/p2p/QmSaT2NnWddF4e2WVWSPz22mp2dYXFnESF4vRqGuBB4SFU/p2p-circuit/p2p/" + answer.answer[i]);
+        }
+      }
     //}, 100);
   });
 
